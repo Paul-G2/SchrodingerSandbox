6626f5c41b2890a9a1f1069f02981b6ea5e68862
Paul-G2 <Paul-G2@users.noreply.github.com>
2023-10-06 21:19:24 -0400

    Title animation.

--- Ui/Topbar.js
+++ Ui/Topbar.js
@@ -16,15 +16,61 @@ class Topbar
     { 
         this.app = app;
         this.parentDiv = parentDiv;   
+        this.animating = false;
+        this.umlautOpacity = 0.9;
+        this.deltaOpacitySign = 1;
 
-        this.titleSpan = UiUtils.CreateElement('span', 'title-span', 
+        this.titleSpan1 = UiUtils.CreateElement('span', 'title-span', 
             this.parentDiv, {top:'0%', height:'100%', left:'0px', width:'100%', 
             display:'flex', alignItems:'center', justifyContent:'center',
-            color:'#000000', fontFamily:'Arial', fontWeight:'bold', 
+            color:'rgba(0,0,0, 0.9)', fontFamily:'Arial', fontWeight:'bold', 
             fontStyle:'italic', fontSize: '4.25vh'}
         ); 
-        this.titleSpan.innerHTML = 'Schr' + String.fromCharCode(246) + 'dinger Sandbox';
-        this.titleSpan.className += " noselect"
+        this.titleSpan1.innerHTML = 'Schr' + String.fromCharCode(246) + 'dinger Sandbox'; // umlaut
+        this.titleSpan1.className += " noselect"
+
+        this.titleSpan2 = UiUtils.CreateElement('span', 'title-span', 
+            this.parentDiv, {top:'0%', height:'100%', left:'0px', width:'100%', 
+            display:'flex', alignItems:'center', justifyContent:'center',
+            color:'rgba(0,0,0, 1.0)', fontFamily:'Arial', fontWeight:'bold', 
+            fontStyle:'italic', fontSize: '4.25vh'}
+        ); 
+        this.titleSpan2.innerHTML = 'Schrodinger Sandbox'; // no umlaut
+        this.titleSpan2.className += " noselect"
+    }
+
+
+    /**
+     * Starts the title animation
+     */
+    startAnimation()
+    {
+        if (!this.animating)
+        {
+            this.animating = true;
+            const animLoop = function() {
+                if ((this.umlautOpacity >= 0.9) || (this.umlautOpacity <= 0.0)) { this.deltaOpacitySign *= -1; }
+                this.umlautOpacity += this.deltaOpacitySign*(1/60);
+                this.titleSpan1.style.color = 'rgba(0,0,0, ' + this.umlautOpacity.toString() + ')';
+                if (this.animating) {    
+                    requestAnimationFrame(animLoop);
+                } else {
+                    this.animating = false;
+                }
+            }.bind(this);
+            requestAnimationFrame(animLoop);
+        }
+    }
+
+    /**
+     * Stops the title animation
+     */
+    stopAnimation()
+    {
+        this.animating = false;
+        this.umlautOpacity = 0.9;
+        this.deltaOpacitySign = 1;
+        this.titleSpan1.style.color = 'rgba(0,0,0, ' + this.umlautOpacity.toString() + ')';
     }
     
 };
